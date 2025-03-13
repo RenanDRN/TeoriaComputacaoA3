3683541b08faf129a2aeb2089607c3e3e53cb9d9
Caslu-Parra <lucasparra2@outlook.com>
2025-03-13 18:56:34 -0300

    feat: carga inicial do projeto backend

---
+++ backend/src/test/java/usjt/grupo19/sweetmachine/SweetmachineApplicationTests.java
@@ -0,0 +1,13 @@
+package usjt.grupo19.sweetmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetmachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
