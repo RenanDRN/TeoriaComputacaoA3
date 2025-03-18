9bfeae254217b4f304f125294eb39f8248703700
Aline Cerqueira de Andrade <alinewacerqueira@gmail.com>
2025-03-17 21:59:13 -0300

    feat: adiciona telas menu e máquina e atualiza dependencias-pom

---
+++ src/main/java/com/mycompany/projetoa3comp/ProjetoA3Comp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoa3comp;
+import Telas.TelaMenu;
+
+/**
+ *
+ * @author Aline
+ */
+public class ProjetoA3Comp {
+
+    public static void main(String[] args) {
+        TelaMenu telamenu = new TelaMenu();
+        telamenu.setVisible(true);
+    }
+}
